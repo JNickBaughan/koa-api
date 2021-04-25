5e7900f087a93d7ea5f9ecae36de7e9f269f0aa2
JNickBaughan <JNickBaughan@gmail.com>
2021-04-25 12:34:00 -0400

    WIP - add handlers using mountebank mocks

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const Koa = require("koa");
+const axios = require("axios");
 const OpenAPIBackend = require("openapi-backend").default;
+const bodyparser = require("koa-bodyparser");
+const cors = require("@koa/cors");
 
 const app = new Koa();
+app.use(bodyparser({ enableTypes: ["json"] }));
+app.use(cors());
+
+const getUser = async () => {
+  const response = await axios.get("http://localhost:1001/user");
+  return response.data;
+};
 
 const api = new OpenAPIBackend({
   definition: "./openapi.yml",
   strict: true,
   handlers: {
-    getUser: (ctx, koaCtx) => {
-      console.dir(ctx);
-      console.dir(koaCtx);
-      koaCtx.body = {
-        user: "bob",
-      };
+    getUser: async (ctx, koaCtx) => {
+      try {
+        return getUser().then((data) => {
+          console.dir(data);
+          koaCtx.body = data;
+          koaCtx.status = 200;
+        });
+      } catch (err) {
+        console.log("error");
+        koaCtx.app.emit("error", err, koaCtx);
+      }
     },
   },
 });
